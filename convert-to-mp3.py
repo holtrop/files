496214cd71dc3ab9dc30caf88b03cbd92a2ec7bf
Josh Holtrop <jholtrop+git@gmail.com>
2011-04-16 15:22:31 -0400

    convert-to-mp3.py: can specify bitrate optionally

--- convert-to-mp3.py
+++ convert-to-mp3.py
@@ -2,12 +2,27 @@
 
 import os
 import sys
+import getopt
 from subprocess import *
 
 def main(argv):
-    for f in argv:
+    bitrate = 128
+    try:
+        opts, args = getopt.getopt(argv[1:], "b:")
+    except getopt.GetoptError:
+        usage()
+        return 2
+
+    for opt, arg in opts:
+        if opt == "-b":
+            bitrate = arg
+        else:
+            usage()
+            return 2
+
+    for f in args:
         if os.path.isfile(f):
-            Popen(['lame', '-v', f, f + '.mp3']).wait()
+            Popen(['lame', '-v', '-b', bitrate, f, f + '.mp3']).wait()
 
 if __name__ == "__main__":
     main(sys.argv)
